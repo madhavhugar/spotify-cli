1cdd1eeeaed9246dc44b9f49566e406391f54956
Madhav Hugar <madhav.hugar@gmail.com>
2021-01-29 16:16:17 +0100

    implement spotify client

---
+++ go.mod
@@ -0,0 +1,8 @@
+module spotify-cli
+
+go 1.15
+
+require (
+	github.com/zmb3/spotify v1.1.0
+	golang.org/x/oauth2 v0.0.0-20210126194326-f9ce19ea3013
+)
---
+++ spotify/spotify.go
@@ -0,0 +1,90 @@
+package spotify
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+// new returns an authenticated spotify client
+func new() (spotify.Client, error) {
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_CLIENT_SECRET"),
+		TokenURL:     spotify.TokenURL,
+	}
+	token, err := config.Token(context.Background())
+	if err != nil {
+		return spotify.Client{}, err
+	}
+
+	client := spotify.Authenticator{}.NewClient(token)
+
+	return client, nil
+}
+
+func Albums(name string) []spotify.SimpleAlbum {
+	client, err := new()
+	if err != nil {
+		log.Fatalf("error instantiating spotify client")
+	}
+	results, err := client.Search(name, spotify.SearchTypeAlbum)
+	if err != nil {
+		log.Fatalf("could not search album name %s: %v", name, err)
+	}
+	return results.Albums.Albums
+}
+
+func Artists(name string) []spotify.FullArtist {
+	client, err := new()
+	if err != nil {
+		log.Fatalf("error instantiating spotify client")
+	}
+	results, err := client.Search(name, spotify.SearchTypeArtist)
+	if err != nil {
+		log.Fatalf("could not search artist name %s: %v", name, err)
+	}
+	return results.Artists.Artists
+}
+
+func Tracks(name string) []spotify.FullTrack {
+	client, err := new()
+	if err != nil {
+		log.Fatalf("error instantiating spotify client")
+	}
+	results, err := client.Search(name, spotify.SearchTypeTrack)
+	if err != nil {
+		log.Fatalf("could not search track name %s: %v", name, err)
+	}
+	return results.Tracks.Tracks
+}
+
+func Playlists(name string) []spotify.SimplePlaylist {
+	client, err := new()
+	if err != nil {
+		log.Fatalf("error instantiating spotify client")
+	}
+	results, err := client.Search(name, spotify.SearchTypePlaylist)
+	if err != nil {
+		log.Fatalf("could not search playlist name %s: %v", name, err)
+	}
+	return results.Playlists.Playlists
+}
+
+func All(name string) ([]spotify.FullTrack, []spotify.SimpleAlbum, []spotify.FullArtist, []spotify.SimplePlaylist) {
+	client, err := new()
+	if err != nil {
+		log.Fatalf("error instantiating spotify client")
+	}
+	results, err := client.Search(
+		name,
+		spotify.SearchTypePlaylist|spotify.SearchTypeArtist|spotify.SearchTypeTrack|spotify.SearchTypeAlbum,
+	)
+	if err != nil {
+		log.Fatalf("could not search playlist name %s: %v", name, err)
+	}
+	return results.Tracks.Tracks, results.Albums.Albums, results.Artists.Artists, results.Playlists.Playlists
+}
